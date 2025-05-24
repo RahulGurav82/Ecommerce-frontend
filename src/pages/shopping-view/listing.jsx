5c73cf2342f7db27ad1e542b4d8b14905dd65f9b
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-05-24 19:41:02 +0530

    shop view done

--- src/pages/shopping-view/listing.jsx
+++ src/pages/shopping-view/listing.jsx
@@ -1,4 +1,5 @@
 import ProductFillter from "@/components/shopping-view/fillter";
+import ProductDetailsDialog from "@/components/shopping-view/product-details";
 import ShoppingProductTile from "@/components/shopping-view/product-tile";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,53 +10,91 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { sortOptions } from "@/config";
-import { fetchAllFilteredProducts } from "@/store/shop/product-slice";
+import {
+  fetchAllFilteredProducts,
+  fetchProductDetails,
+} from "@/store/shop/product-slice";
 import { ArrowUpDownIcon } from "lucide-react";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+
+const createSearchParamsHelper = (filtersParams) => {
+  const queryParams = [];
+  for (const [key, value] of Object.entries(filtersParams)) {
+    const paramValue = value.join(",");
+
+    queryParams.push(`${key}=${encodeURIComponent(paramValue)}`);
+  }
+
+  return queryParams.join("&");
+};
 
 const ShoppingListing = () => {
   const dispatch = useDispatch();
-  const { productList } = useSelector((state) => state.shoppingProducts);
-  const [filters, setFilters] = useState({})
-  const [sort, setSort] = useState(null)
+  const { productList, productDetails } = useSelector(
+    (state) => state.shoppingProducts
+  );
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false)
 
   const handleSort = (value) => {
     setSort(value);
-  }
+  };
 
   const handleFilter = (getSectionId, getCurrentOption) => {
-    console.log(getSectionId, getCurrentOption)
+    console.log(getSectionId, getCurrentOption);
 
-    let cpyFilters = { ...filters};
+    let cpyFilters = { ...filters };
     const indexOfCurrentSection = Object.keys(cpyFilters).indexOf(getSectionId);
 
-    if(indexOfCurrentSection === -1) {
+    if (indexOfCurrentSection === -1) {
       cpyFilters = {
-        ...cpyFilters, 
-        [getSectionId] : [getCurrentOption]
-      }
+        ...cpyFilters,
+        [getSectionId]: [getCurrentOption],
+      };
     } else {
-      const indexOfCurrentOption = cpyFilters[getSectionId].indexOf(getCurrentOption)
-      if(indexOfCurrentOption === -1) cpyFilters[getSectionId].push(getCurrentOption)
-        else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1)
+      const indexOfCurrentOption =
+        cpyFilters[getSectionId].indexOf(getCurrentOption);
+      if (indexOfCurrentOption === -1)
+        cpyFilters[getSectionId].push(getCurrentOption);
+      else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1);
     }
 
     setFilters(cpyFilters);
-    sessionStorage.setItem('filters', JSON.stringify(cpyFilters));
-  }
+    sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+  };
+
+  const handleGetProductDetails = (id) => {
+    dispatch(fetchProductDetails(id));
+  };
+
+  useEffect(() => {
+    if(productDetails !== null) setOpenDetailsDialog(true)
+  }, [productDetails])
 
   useEffect(() => {
     setSort("price-lowtohigh");
-    setFilters(JSON.parse(sessionStorage.getItem('filters')) || {} );
+    setFilters(JSON.parse(sessionStorage.getItem("filters")) || {});
   }, []);
 
   useEffect(() => {
-    dispatch(fetchAllFilteredProducts());
-  }, [dispatch]);
+    if (filters && Object.keys(filters).length > 0) {
+      const createQoueryString = createSearchParamsHelper(filters);
+      setSearchParams(new URLSearchParams(createQoueryString));
+    }
+  }, [filters]);
 
- 
+  useEffect(() => {
+    if (filters !== null && sort !== null)
+      dispatch(
+        fetchAllFilteredProducts({ filterParams: filters, sortParams: sort })
+      );
+  }, [dispatch, sort, filters]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
       <ProductFillter filters={filters} handleFilter={handleFilter} />
@@ -80,10 +119,13 @@ const ShoppingListing = () => {
               <DropdownMenuContent align="end" className="w-[200px]">
                 <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
                   {sortOptions.map((sortItem) => (
-                    <DropdownMenuRadioItem value={sortItem.id} key={sortItem.id}>
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
                       {sortItem.label}
                     </DropdownMenuRadioItem>
-                  ))}               
+                  ))}
                 </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
@@ -92,11 +134,15 @@ const ShoppingListing = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
           {productList && productList.length > 0
             ? productList.map((productItem) => (
-                <ShoppingProductTile product={productItem} />
+                <ShoppingProductTile
+                  product={productItem}
+                  handleGetProductDetails={handleGetProductDetails}
+                />
               ))
             : null}
         </div>
       </div>
+      <ProductDetailsDialog open={openDetailsDialog} setOpen={setOpenDetailsDialog} productDetails={productDetails} />
     </div>
   );
 };
--- src/store/shop/product-slice/index.jsx
+++ src/store/shop/product-slice/index.jsx
@@ -1,16 +1,35 @@
 import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { URLSearchParams } from "url";
 
 const initialState = {
   isLoading: false,
   productList: [],
+  productDetails : null
 };
 
 export const fetchAllFilteredProducts = createAsyncThunk(
   "/products/fetchAllProducts",
-  async () => {
+  async ({filterParams, sortParams}) => {
+
+    const query = new URLSearchParams({
+      ...filterParams,
+      sortBy : sortParams
+    })
     const result = await axios.get(
-      "http://localhost:5000/api/shop/products/get"
+      `http://localhost:5000/api/shop/products/get?${query}`
+    );
+
+    return result?.data;
+  }
+);
+
+export const fetchProductDetails = createAsyncThunk(
+  "/products/fetchProductDetails",
+  async (id) => {
+
+    const result = await axios.get(
+      `http://localhost:5000/api/shop/products/get/${id}`
     );
 
     return result?.data;
@@ -33,6 +52,16 @@ const ShoppingProductSlice = createSlice({
       .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
         state.isLoading = false;
         state.productList = [];
+      }).addCase(fetchProductDetails.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = action.payload.data;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = null;
       });
   },
 });
